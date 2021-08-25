a466ce84f629e39ac499af71b3f51143835c3109
Defite <me@defite.ru>
2021-08-25 22:52:02 +0300

    fix(nav): fix animation transition

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useEffect, useRef } from 'react'
 import Link from 'next/link'
 import styles from './nav.module.css'
 
@@ -12,10 +12,36 @@ const Nav: FunctionComponent<INav> = ({ opened, setOpen }) => {
     ? 'duration-200 ease-out opacity-100 transform scale-100'
     : 'duration-200 ease-in opacity-0 transform scale-95'
 
+  const navRef = useRef<HTMLElement>(null)
+
   const onLinkClick = () => setTimeout(() => setOpen(false), 500)
+
+  useEffect(() => {
+    let timer: any = null
+
+    const nav = navRef.current
+
+    window.addEventListener('resize', function () {
+      if (timer) {
+        clearTimeout(timer)
+        timer = null
+      } else {
+        nav?.classList.add('transition-none')
+      }
+      timer = setTimeout(() => {
+        nav?.classList.remove('transition-none')
+        timer = null
+      }, 100)
+    })
+
+    return () => {
+      clearTimeout(timer)
+    }
+  })
 
   return (
     <nav
+      ref={navRef}
       className={`${styles.nav} ${navClass} md:opacity-100 md:transform-none`}
     >
       <ul className={styles.navList}>
