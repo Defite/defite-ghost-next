5d47d8b4b0caa02350e9104c1e0f480a6f35a71c
Defite <me@defite.ru>
2021-08-26 13:56:28 +0300

    feat(postheader): add align prop

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,6 +8,7 @@ const AboutPage: React.FunctionComponent = () => {
         title="About me"
         description="A few words about me and this web page"
         theme="background"
+        align="center"
       />
       <PostLayout className="prose md:prose-lg mx-auto <md:px-5">
         Hey again! You probably already know who I am?
